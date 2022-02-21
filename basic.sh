54b117f5cd47f5b72ce7bab6a0c5c900744cfd29
sadamalsharabi <44142745+sadamshr3be@users.noreply.github.com>
2022-02-21 16:58:01 +0300

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,146 @@
+echo -e "\e[1;31mThe script is  made by ShuBhamg0sain"
+echo ""
+echo -e "\e[1;31m follow me on Instagram https://instagram.com/sadam_alsharabi?igshid=1wlndj3lf8pdm"
+echo ""
+echo ""
+echo ""
+echo -e "\e[1;32m Installtion process is started" 
+echo ""
+echo -e "\e[1;32m 𝑻𝒆𝒓𝒎𝒖𝒙 𝑩𝒂𝒔𝒊𝒄 𝑪𝒐𝒎𝒎𝒂𝒏𝒅𝒔"
+apt update -y 
+apt upgrade -y 
+apt install git -y  
+apt install python2 -y 
+apt install php 
+apt install openssh 
+apt install curl 
+apt install python 
+apt install nano 
+apt install toilet 
+apt install perl 
+apt install nmap 
+apt install clang 
+apt install bash 
+apt install w3m  
+apt install hydra 
+apt install figlet  
+apt install tar 
+apt install root-repo
+apt install unstable-repo
+apt install x11-repo
+apt install unzip 
+apt install tor 
+apt install wget 
+apt install wcalc 
+apt install bmon 
+apt install proot 
+apt install toilet 
+apt install net-tools &&
+apt install golang termux-chroot
+apt install cmatrix 
+apt install macchanger 
+apt install ruby 
+apt install fish 
+apt install help
+pip2 install --upgrade pip
+pip2 install requests 
+pip2 install Mechanize 
+pip2 install termcolor 
+gem install lolcat 
+pip install -r requirements.txt
+pip2 install requirements.txt
+pip2 install mechnaize
+apt update -y
+apt upgrade -y 
+apt install git -y 
+apt install python2 -y 
+apt install php 
+apt install openssh 
+apt install curl 
+apt install python 
+apt install nano 
+apt install toilet 
+apt install perl 
+apt install nmap 
+apt install clang 
+apt install bash 
+apt install w3m  
+apt install hydra 
+apt install figlet  
+apt install tar 
+apt install unzip 
+apt install tor 
+apt install wget 
+apt install wcalc 
+apt install bmon 
+apt install proot 
+apt install toilet 
+apt install net-tools 
+apt install golang termux-chroot 
+apt install cmatrix 
+apt install macchanger 
+apt install ruby
+apt install fish 
+apt install help
+pip2 install --upgrade pip 
+pip2 install requests 
+pip2 install Mechanize 
+pip2 install termcolor
+gem install lolcat
+apt install bash
+apt update -y
+apt upgrade -y
+apt install python -y
+apt install python2 -y
+apt install python2-dev -y
+apt install python3 -y
+apt install java -y
+apt install fish -y
+apt install ruby -y
+apt install help -y
+apt install git -y
+apt install host -y
+apt install php -y
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install nano -y
+apt install w3m -y
+apt install havij -y
+apt install hydra -y
+apt install figlet -y
+apt install cowsay -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install google -y
+apt install sudo -y
+apt install wget -y
+apt install wireshark -y
+apt install wgetrc -y 
+apt install wcalc -y
+apt install bmon -y
+apt install vpn -y
+apt install unrar -y
+apt install toilet -y
+apt install proot -y
+apt install net-tools -y
+apt install golang -y
+apt install chroot -y
+apt install macchanger -y
+apt install openssl -y
+apt install cmatrix -y
+apt install openssh -y
+apt install wireshark -y
+termux-setup-storage -y
+apt install macchanger -y
+echo ""
+echo -e "\e[1;32mAll basic packages and tools is installed"
+echo ""
+echo -e "\e[1;32mBe a Hacker...."
+echo ""
+echo "follow me on https://instagram.com/sadam_alsharabi?igshid=1wlndj3lf8pdm"
+exit
